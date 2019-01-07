f1585ea884b6a55f93ce56b18dd48710f4d6b235
mohamed <mohamed@faress-mac-3.local>
2019-01-07 09:02:41 +0200

    final

--- MemeMe-1.0-/memedEditViewController.swift
+++ MemeMe-1.0-/memedEditViewController.swift
@@ -4,49 +4,45 @@
 //
 //  Created by mohamed on 1/4/19.
 //  Copyright © 2019 mohamed. All rights reserved.
-
-
+//
 
 import UIKit
 
+
 class memedEditViewController: UIViewController ,UINavigationControllerDelegate,UIImagePickerControllerDelegate,UITextFieldDelegate{
     
-    override var prefersStatusBarHidden: Bool{
-        return true
-    }
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var toolBar: UIToolbar!
-    @IBOutlet weak var bottonTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var photoLibraryButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
-  //  let memeTextAttributes:[NSAttributedString.Key.Any]=[
+    //  let memeTextAttributes:[NSAttributedString.Key.Any]=[
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
-        NSAttributedString.Key.foregroundColor:UIColor.white,
-    
+        NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1),
+        
         NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
         
-        NSAttributedString.Key.strokeWidth: -4.0,
-      
+        NSAttributedString.Key.strokeWidth: -4.0
+        
     ]
     
- 
-  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         func stylizeTextField(textField:UITextField){
             textField.defaultTextAttributes=memeTextAttributes
-            bottonTextField.text="botton"
-        topTextField.text="TOP"
-        textField.textAlignment = .center
-        textField.delegate=self
-            
+            bottomTextField.text="BOttom"
+            topTextField.text="TOP"
+            textField.textAlignment = .center
+            textField.delegate=self
         }
         
-       stylizeTextField(textField: bottonTextField)
+        stylizeTextField(textField: bottomTextField)
         stylizeTextField(textField: topTextField)
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -55,9 +51,12 @@ class memedEditViewController: UIViewController ,UINavigationControllerDelegate,
         self.subscipToKeyboardNotication()
         self.unsubscipToKeyboardNotication()
     }
- 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        if bottonTextField.text=="TOP"||topTextField.text=="BOttom"{
+        if bottomTextField.text=="TOP"||topTextField.text=="BOttom"{
             textField.text=""
         }
     }
@@ -68,9 +67,9 @@ class memedEditViewController: UIViewController ,UINavigationControllerDelegate,
     }
     @ objc func keyboardWillShowNotification(_ notification:Notification){
         if topTextField.isFirstResponder{
-        
-      view.frame.origin.y = -getKeyboardHeight(notification)
-    }
+            
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
     }
     func getKeyboardHeight(_ notification:Notification) -> CGFloat {
         
@@ -87,17 +86,15 @@ class memedEditViewController: UIViewController ,UINavigationControllerDelegate,
     }
     @objc func keyboardWillHideNotification(_ notification:Notification){
         if topTextField.isFirstResponder{
-        view.frame.origin.y=0
-        }
+            view.frame.origin.y=0
+        }}
+    func unsubscipToKeyboardNotication(){
         
-    }
-     func unsubscipToKeyboardNotication(){
-       
-      NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHideNotification(_:)), name:UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHideNotification(_:)), name:UIResponder.keyboardWillHideNotification, object: nil)
     }
     func save(){
         let memedImage = generatmemmedImage()
-        _ = Meme(top: topTextField.text!, bottom: bottonTextField.text!, image: imageView.image, memedImage:memedImage)
+        _ = Meme(top: topTextField.text!, bottom: bottomTextField.text!, image: imageView.image, memedImage:memedImage)
         
         
     }
@@ -105,7 +102,7 @@ class memedEditViewController: UIViewController ,UINavigationControllerDelegate,
         // TODO: Hide toolbar and navbar
         navigationBar.isHidden=true
         toolBar.isHidden=true
-         // Render view to an image
+        // Render view to an image
         UIGraphicsBeginImageContext(self.view.frame.size)
         view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
         let memmedImage:UIImage=UIGraphicsGetImageFromCurrentImageContext()!
@@ -121,12 +118,13 @@ class memedEditViewController: UIViewController ,UINavigationControllerDelegate,
     @IBAction func imagePicker(_ sender: Any) {
         //this for preent photo from library
         //refrence from
-      
-         pickImage(sourceType:.photoLibrary)
+        
+        pickImage(sourceType:.photoLibrary)
         
     }
-    @IBAction func cameraButtonAction(_ sender: Any) {
-       pickImage(sourceType:.camera)
+    @IBAction func cameraButtonAction(_ sender: Any)
+    {
+        pickImage(sourceType:.camera)
         
     }
     func pickImage(sourceType: UIImagePickerController.SourceType){
@@ -134,30 +132,26 @@ class memedEditViewController: UIViewController ,UINavigationControllerDelegate,
         imagePiker.delegate=self
         
         imagePiker.sourceType = sourceType
-            
-       
-        imagePiker.sourceType = .camera
+        
+        
         self.present(imagePiker,animated: true,completion: nil)
-       
+        
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-      let image=info[UIImagePickerController.InfoKey.originalImage]as?UIImage
+        let image=info[UIImagePickerController.InfoKey.originalImage]as?UIImage
         imageView.image=image
         
-     
+        
         self.dismiss(animated: true, completion: nil)
         
     }
     
     @IBAction func shareButtonAction(_ sender: Any) {
-     
         let memedImage = generatmemmedImage()
         let activityController = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
         activityController.completionWithItemsHandler = { activity, success, items, error in
             if success{
                 self.save()
-            } else {
-                //Do nothing
             }
             self.dismiss(animated: true, completion: nil)
         }
@@ -166,12 +160,13 @@ class memedEditViewController: UIViewController ,UINavigationControllerDelegate,
         
     }
     @IBAction func cancelButtonAction(_ sender: Any) {
-        //self.dismissViewControllerAnimated(true, completion: nil)
+        
         topTextField.text = "TOP"
-        bottonTextField.text = "BOTTOM"
+        bottomTextField.text = "BOTTOM"
         self.imageView.image = nil
+        
     }
     
     
-   
+    
 }
